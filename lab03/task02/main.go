37316692ddfdb5fc2d27646fb06a6f3cf07efd08
Maksim Grekov <maxvivd@gmail.com>
2023-09-22 18:15:01 +0300

    feat

--- lab03/task02/main.go
+++ lab03/task02/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"ParallelLabs/lab03/task01/utils"
 	"fmt"
-	"github.com/schollz/progressbar/v3"
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"math"
@@ -11,34 +10,73 @@ import (
 	"runtime"
 )
 
-func toValues(x []uint64) plotter.Values {
-	values := make(plotter.Values, len(x))
+//func toValues(x []uint64) plotter.Values {
+//	values := make(plotter.Values, len(x))
+//
+//	for i := range x {
+//		values[i] = float64(x[i])
+//	}
+//
+//	return values
+//}
+
+func toBars(x []uint64, n uint64) plotter.Values {
+	h := uint64(math.MaxUint64) / n
+	values := make(plotter.Values, n)
 
 	for i := range x {
-		values[i] = float64(x[i])
+		values[x[i]/h] += 1
 	}
 
 	return values
 }
 
-func main() {
-	bar := progressbar.Default(int64(4))
+func histogram(x []uint64) {
+	p := plot.New()
+	bars, _ := plotter.NewBarChart(toBars(x, 50), 5)
+	p.Add(bars)
+	err := p.Save(500, 500, fmt.Sprintf("lab03/task02/plots/histogram/plot%d.png", len(x)))
+	if err != nil {
+		panic(err)
+	}
+}
 
-	for i := 2; i <= 5; i++ {
+func toScatters(x []uint64, n int) plotter.XYs {
+	xys := make(plotter.XYs, 0, n)
+
+	for i := range x {
+		xys = append(xys, plotter.XY{
+			X: float64(x[i]),
+			Y: math.Log(float64(n)),
+		})
+	}
+
+	return xys
+}
+
+func scattering(p *plot.Plot, x []uint64, n int) {
+	s, _ := plotter.NewScatter(toScatters(x, n))
+	p.Add(s)
+}
+
+func main() {
+	scatterPlot := plot.New()
+
+	for i := 2; i <= 7; i++ {
 		runtime.GC()
 		n := int(math.Pow(float64(10), float64(i)))
 		x := utils.Kiss(n, [4]uint64{rand.Uint64(), rand.Uint64(), rand.Uint64(), rand.Uint64()})
 
-		p := plot.New()
-		bars, _ := plotter.NewBarChart(toValues(x), 1)
-		p.Add(bars)
-		err := p.Save(2000, 500, fmt.Sprintf("lab03/task02/plots/plot%d.png", n))
-		if err != nil {
-			panic(err)
+		histogram(x)
+
+		if i < 5 {
+			scattering(scatterPlot, x, n)
 		}
-		_ = bar.Add(1)
+
 	}
 
-	_ = bar.Close()
-
+	err := scatterPlot.Save(2000, 200, "lab03/task02/plots/scatter/plot.png")
+	if err != nil {
+		panic(err)
+	}
 }
